3b2e0ea44e331e2262fae957f06142bec1685eae
unknown <ssgsj.carranza@gmail.com>
2021-09-07 18:55:55 -0500

    added selectors for each action to grab them from data layer

--- slices/navSlice.js
+++ slices/navSlice.js
@@ -6,12 +6,26 @@ const initialState = {
     travelTimeInformation: null,
 }
 
+//DATA LAYER BELOW
 export const navSlice = createSlice({
     name: 'nav',
     initialState,
     reducer: {
         setOrigin: (state, action) => {
             state.origin = action.payload;
-        }
-    }
-})
+        },
+        setDestination: (state, action) => {
+            state.destination = action.payload;
+        },
+        setTravelTimeInformation: (state, action) => {
+            state.travelTimeInformation = action.payload;
+        },
+    },
+});
+
+export const {setOrigin, setDestination, setTravelTimeInformation} = navSlice.actions;
+
+//SELECTORS (GRAB FROM DATA LAYER)
+export const selectOrigin = (state) => state.nav.origin;
+export const selectDestination = (state) => state.nav.destination;
+export const selectTravelTimeInformation = (state) => state.nav.travelTimeInformation;
