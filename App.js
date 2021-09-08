04259f7466fc8ff80ef9cfc4a78ce5b33aa25290
unknown <ssgsj.carranza@gmail.com>
2021-09-07 19:12:31 -0500

    created screen folder, and home screen component, begining work on home screen component

--- App.js
+++ App.js
@@ -2,16 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
 //SET UP REDUX
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>My first android app Uber clone!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <HomeScreen />
     </Provider>
   );
 }
