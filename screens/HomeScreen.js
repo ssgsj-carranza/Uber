4bd0e36fd617d61f4831d740deb17b6ef64b0919
unknown <ssgsj.carranza@gmail.com>
2021-09-07 19:49:38 -0500

    made components folder and added navoptions folder to it, created navoptions function

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
 import tw from 'tailwind-react-native-classnames';
+import NavOptions from './components/NavOptions';
 
 const HomeScreen = () => {
     return (
-        <SafeAreaView style={tw `bg-white h-full`}>
+        <SafeAreaView style={tw`bg-white h-full`}>
             <View style={tw`p-5`}>
                 <Image
                     style={{
@@ -16,6 +17,7 @@ const HomeScreen = () => {
                         uri: 'https://links.papareact.com/gzs',
                     }}
                 />
+                <NavOptions />
             </View>
         </SafeAreaView>
     );
---
+++ screens/components/NavOptions.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const NavOptions = () => {
+    return (
+        <View>
+            <Text>Options</Text>
+        </View>
+    )
+}
+
+export default NavOptions
+
+
