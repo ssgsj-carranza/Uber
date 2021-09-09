3ad4d5fa8ccb62bc1862a210b5656d9b1e7520cf
unknown <ssgsj.carranza@gmail.com>
2021-09-08 21:27:06 -0500

    solved unable to resolve module issue, manually went into node modules folder, committed out import from react native safe area context. For some reason cleaning the cache and deleting folder didnt get rid off import. issuesolved

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
+import { SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native'
 import tw from 'tailwind-react-native-classnames';
 import NavOptions from './components/NavOptions';
 
