e001f3e82f11789cb420518e6c9075248a397735
unknown <ssgsj.carranza@gmail.com>
2021-09-09 21:06:02 -0500

    deleted safe are context provider from expo node modules folder, keeps hitting me with exception, wrapped home screen component in stack navigator

--- App.js
+++ App.js
@@ -4,12 +4,24 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 //SET UP REDUX
 
 export default function App() {
+  const Stack = createStackNavigator();
+
   return (
     <Provider store={store}>
-      <HomeScreen />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen 
+            name='HomeScreen'
+            component={HomeScreen}
+          />
+          </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 }
--- screens/components/NavOptions.js
+++ screens/components/NavOptions.js
@@ -54,3 +54,6 @@ export default NavOptions
 //npm install react-native-elements
 //npm install react-native-vector-icons
 //npm install react-native-safe-area-context (uninstalled)
+//npm install react-navigation/native
+//expo install react-native-gesture-handler react-native-reanimated react-native-screens @react-native-community/masked-view
+//npm install @react-navigation/stack
