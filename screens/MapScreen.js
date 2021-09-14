275a0bbf92361d4e334d0da77f2a9b0c74c8315d
unknown <ssgsj.carranza@gmail.com>
2021-09-14 18:53:08 -0500

    created navigate card component to display that different types on available transports, rendered stack navigator in map screen

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -3,8 +3,12 @@ import { StyleSheet, Text, View } from 'react-native'
 import tw from 'tailwind-react-native-classnames';
 import Map from './components/Map';
 import MapView from 'react-native-maps';
+import { createStackNavigator } from '@react-navigation/stack';
+import NavigateCard from './components/NavigateCard';
 
 const MapScreen = () => {
+    const Stack = createStackNavigator();
+
     return (
         <View>
             <View style={tw`h-1/2`}>
@@ -12,7 +16,17 @@ const MapScreen = () => {
             </View>
                 
             
-            <View style={tw`h-1/2`}></View>
+            <View style={tw`h-1/2`}>
+                <Stack.Navigator>
+                    <Stack.Screen 
+                        name='NavigateCard'
+                        component={NavigateCard}
+                        options={{
+                            headerShown: false,
+                        }}
+                    />
+                </Stack.Navigator>
+            </View>
         </View>
     )
 }
