0d75206aca00258c0c2eff162e9e54f8a9d66660
unknown <ssgsj.carranza@gmail.com>
2021-09-13 19:24:05 -0500

    pulled in dispatch to change setOrigin data, pulling in location and the description from the location

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,9 +5,11 @@ import tw from 'tailwind-react-native-classnames';
 import NavOptions from './components/NavOptions';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import {GOOGLE_MAPS_KEY} from '@env';
+import { useDispatch } from 'react-redux';
+import {setDestination, setOrigin} from '../slices/navSlice';
 
 const HomeScreen = () => {
-    
+    const dispatch = useDispatch();
 
     return (
         <SafeAreaView style={tw`bg-white h-full`}>
@@ -34,7 +36,11 @@ const HomeScreen = () => {
                     minLength={2}
                     enablePoweredByContainer={false}
                     onPress={(data, details = null) => {
-
+                        dispatch(setOrigin({
+                            location: details.geometry.location,
+                            description: data.description,
+                        }))
+                        dispatch(setDestination(null))
                     }}
                     fetchDetails={true}
                     returnKeyType={'search'}
