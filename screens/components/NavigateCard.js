3cb068139dbdb2246a6fc58a197eb803a7c61c0c
unknown <ssgsj.carranza@gmail.com>
2021-09-14 19:09:07 -0500

    added styling to where to container

--- screens/components/NavigateCard.js
+++ screens/components/NavigateCard.js
@@ -1,11 +1,38 @@
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
 import React from 'react'
 import tw from 'tailwind-react-native-classnames'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import {GOOGLE_MAPS_KEY} from "@env";
 
 const NavigateCard = () => {
     return (
         <SafeAreaView style={tw`bg-white flex-1`}>
             <Text style={tw`text-center py-5 text-xl`}>Greetings and Welcome</Text>
+            <View style={tw`border-t border-gray-200 flex-shrink`}>
+                <View>
+                    <GooglePlacesAutocomplete
+                        styles={{
+                            container: {
+                                flex: 0,
+                                backgroundColor: 'white',
+                                paddingTop: 20,
+                            },
+                            textInput: {
+                                fontSize: 18,
+                                borderRadius: 0,
+                                backgroundColor: '#DDDDDF',
+                            },
+                            textInputContainer: {
+                                paddingHorizontal: 20,
+                                paddingBottom: 0,
+                            },
+                        }}
+                        placeholder='Where to?'
+                        nearbyPlacesAPI='GooglePlacesSearch'
+                        debounce={400}
+                    />
+                </View>
+            </View>
         </SafeAreaView>
     )
 }
