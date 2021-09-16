d9f646dcbca9f1fd87891d28c4c72110fd21a5e4
unknown <ssgsj.carranza@gmail.com>
2021-09-15 19:54:18 -0500

    pulled in keyboardAvoidingView to prevent the keyboard from masking the where to input field, added conditionla to accept changes depending on OS ios or android

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, KeyboardAvoidingView, Platform } from 'react-native';
 import { Provider } from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
@@ -16,22 +16,24 @@ export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen 
-            name='HomeScreen'
-            component={HomeScreen}
-            options={{
-              headerShown:false,
-            }}
-          />
-          <Stack.Screen 
-            name='MapScreen'
-            component={MapScreen}
-            options={{
-              headerShown:false,
-            }}
-          />
-          </Stack.Navigator>
+        <KeyboardAvoidingView style={{flex:1}} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+          <Stack.Navigator>
+            <Stack.Screen 
+              name='HomeScreen'
+              component={HomeScreen}
+              options={{
+                headerShown:false,
+              }}
+            />
+            <Stack.Screen 
+              name='MapScreen'
+              component={MapScreen}
+              options={{
+                headerShown:false,
+              }}
+            />
+            </Stack.Navigator>
+          </KeyboardAvoidingView>
       </NavigationContainer>
     </Provider>
   );
--- screens/components/NavigateCard.js
+++ screens/components/NavigateCard.js
@@ -6,6 +6,7 @@ import {GOOGLE_MAPS_KEY} from "@env";
 import { useDispatch } from 'react-redux';
 import {setDestination} from '../../slices/navSlice';
 import {useNavigation} from '@react-navigation/native';
+import NavFavourites from './NavFavourites';
 
 const NavigateCard = () => {
     const dispatch = useDispatch();
@@ -54,6 +55,7 @@ const NavigateCard = () => {
                         }}
                     />
                 </View>
+                <NavFavourites />
             </View>
         </SafeAreaView>
     )
