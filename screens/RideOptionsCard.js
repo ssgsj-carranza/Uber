5562e33265271147dbf2096a5838e2ee0d7e161c
unknown <ssgsj.carranza@gmail.com>
2021-09-15 20:45:59 -0500

    added dummy data for 3 different type of transport with price multiplier depending on luxury, rendered out in a flatlist, tested and works

--- screens/RideOptionsCard.js
+++ screens/RideOptionsCard.js
@@ -1,8 +1,29 @@
 import React from 'react'
-import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { FlatList, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import tw from 'tailwind-react-native-classnames';
 import {Icon} from 'react-native-elements';
 import {useNavigation} from '@react-navigation/native';
+
+const data = [
+    {
+        id: 'Uber-X-123',
+        title: 'Piggy back',
+        multiplier: 1,
+        image: 'https://links.papareact.com/3pn',
+    },
+    {
+        id: 'Uber-XL-456',
+        title: 'Portkey',
+        multiplier: 1.2,
+        image: 'https://links.papareact.com/5w8',
+    },
+    {
+        id: 'Uber-LUX-789',
+        title: 'Flying Nimbus cloud',
+        multiplier: 1.75,
+        image: 'https://links.papareact.com/7pf',
+    },
+];
 
 const RideOptionsCard = () => {
     const navigation = useNavigation();
@@ -18,6 +39,15 @@ const RideOptionsCard = () => {
                 </TouchableOpacity>
                 <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
             </View>
+            <FlatList 
+                data={data}
+                keyExtractor={(item) => item.id}
+                renderItem={({item}) => (
+                    <TouchableOpacity>
+                        <Text>Car</Text>
+                    </TouchableOpacity>
+                )}
+            />
         </SafeAreaView>
     )
 }
