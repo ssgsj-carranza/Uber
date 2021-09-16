d98b709836549cb33d7005f4d46b122ba63b733e
unknown <ssgsj.carranza@gmail.com>
2021-09-15 20:26:21 -0500

    begining work on select a ride page, will be adding different vehicles and pricing depending on distance

--- screens/RideOptionsCard.js
+++ screens/RideOptionsCard.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import tw from 'tailwind-react-native-classnames';
 
 const RideOptionsCard = () => {
     return (
-        <View>
-            <Text>Choose ride</Text>
-        </View>
+        <SafeAreaView>
+            <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+        </SafeAreaView>
     )
 }
 
