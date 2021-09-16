e2be058382ad3b08ec6ff75316098b93db02f343
unknown <ssgsj.carranza@gmail.com>
2021-09-15 20:39:11 -0500

    added back button to ride options card to navigate back to nav card, added some styling, fixed issue where back arrow wouldnt work by adding z-50 to styling

--- screens/RideOptionsCard.js
+++ screens/RideOptionsCard.js
@@ -1,11 +1,23 @@
 import React from 'react'
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import tw from 'tailwind-react-native-classnames';
+import {Icon} from 'react-native-elements';
+import {useNavigation} from '@react-navigation/native';
 
 const RideOptionsCard = () => {
+    const navigation = useNavigation();
+
     return (
-        <SafeAreaView>
-            <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+        <SafeAreaView style={tw`bg-white flex-grow`}>
+            <View>
+                <TouchableOpacity style={tw`absolute top-3 left-5 p-3 z-50 rounded-full`} onPress={() => navigation.navigate('NavigateCard')}>
+                    <Icon 
+                        name='chevron-left'
+                        type='fontawesome'
+                    />
+                </TouchableOpacity>
+                <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+            </View>
         </SafeAreaView>
     )
 }
