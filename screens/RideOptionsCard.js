1864ad8edef64bf57c0a5091b0f0858d9f7fdd24
unknown <ssgsj.carranza@gmail.com>
2021-09-16 18:45:28 -0500

    reowkred images, begining to render out title and travel time

--- screens/RideOptionsCard.js
+++ screens/RideOptionsCard.js
@@ -7,21 +7,21 @@ import {useNavigation} from '@react-navigation/native';
 const data = [
     {
         id: 'Uber-X-123',
-        title: 'Piggy back',
+        title: 'HM Fly',
         multiplier: 1,
-        image: 'https://links.papareact.com/3pn',
+        image: 'https://images.gameinfo.io/pokemon/256/p6f102.png',
     },
     {
         id: 'Uber-XL-456',
-        title: 'Portkey',
+        title: 'Nimbus 2000',
         multiplier: 1.2,
-        image: 'https://links.papareact.com/5w8',
+        image: 'https://static.wikia.nocookie.net/harrypotter/images/0/0f/Nimbus_2000_1.jpg/revision/latest?cb=20150530185551',
     },
     {
         id: 'Uber-LUX-789',
         title: 'Flying Nimbus cloud',
         multiplier: 1.75,
-        image: 'https://links.papareact.com/7pf',
+        image: 'https://i.redd.it/8kvyrz0t1q651.jpg',
     },
 ];
 
@@ -43,7 +43,7 @@ const RideOptionsCard = () => {
                 data={data}
                 keyExtractor={(item) => item.id}
                 renderItem={({item: {id, title, multiplier, image}}) => (
-                    <TouchableOpacity>
+                    <TouchableOpacity style={tw`flex-row items-center justify-between`}>
                         <Image 
                             style={{
                                 width: 100,
@@ -52,6 +52,10 @@ const RideOptionsCard = () => {
                             }}
                             source={{ uri: image }}
                         />
+                        <View>
+                            <Text>{title}</Text>
+                            <Text>Travel time...</Text>
+                        </View>
                     </TouchableOpacity>
                 )}
             />
